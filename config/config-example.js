a10a9a44846cc60d9e0132dd1ea8391384acf988
catpotattt <catpotalt@gmail.com>
2026-08-12 15:50:11 -0400

    Update config-example.js

--- config/config-example.js
+++ config/config-example.js
@@ -20,7 +20,7 @@ Config.whitelist = [
 // address bar is `Config.routes.client`.
 Config.defaultserver = {
 	id: 'showdown',
-	host: 'sim3.psim.us',
+	host: 'seal-draft-league.com',
 	port: 443,
 	httpport: 8000,
 	altport: 80,
