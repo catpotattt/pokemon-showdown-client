0aab4b9e7a00d9b8e83d0cd0e8fc66258bcfbff9
catpotattt <catpotalt@gmail.com>
2026-08-13 00:32:04 -0400

    build

--- config/config.js
+++ config/config.js
@@ -34,7 +34,7 @@ Config.customcolors = {
 	'zarel': 'aeo'
 };
 /*** Begin automatically generated configuration ***/
-Config.version = "0.11.2";
+Config.version = "0.11.2 (7d65b9d6)";
 
 Config.routes = {
 	root: 'pokemonshowdown.com',
